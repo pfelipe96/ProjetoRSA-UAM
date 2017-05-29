cc4fb6b80ee700407f987498b823218b835a35ab
Paulo Felipe Oliveira Souza <paulofelipeoliveirasouza@MBP-de-Paulo.home>
2017-05-29 18:23:50 -0300

    Add Novo Projeto para o GitHuB
    First Project

---
+++ Lesson2-XML/app/src/main/java/com/example/paulofelipeoliveirasouza/lesson2_xml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paulofelipeoliveirasouza.lesson2_xml;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
